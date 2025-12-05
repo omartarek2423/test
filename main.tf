1bba9c31aecc67b8a6f4e84004376c478973e2c3
omartarek2423 <omartarek200524@gmail.com>
2025-12-06 01:12:55 +0200

    intial

---
+++ main.tf
@@ -0,0 +1,139 @@
+###########################
+# Provider
+###########################
+provider "aws" {
+  region = "us-east-1"
+}
+
+###########################
+# VPC
+###########################
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+###########################
+# Internet Gateway
+###########################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+###########################
+# Public Subnets
+###########################
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+###########################
+# Route Table + Route
+###########################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+###########################
+# Security Group
+###########################
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+###########################
+# EC2 + User Data
+###########################
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public1.id
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  associate_public_ip_address = true
+
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install nginx1 -y
+systemctl enable nginx
+systemctl start nginx
+echo "<h1>Welcome! My name is OMAR TAREK</h1>" > /usr/share/nginx/html/index.html
+EOF
+
+  tags = {
+    Name = "web-server"
+  }
+}
