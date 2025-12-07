af9e84e130ba3067bb44781df163d8c3f3c114fd
omartarek2423 <omartarek200524@gmail.com>
2025-12-07 14:21:53 +0200

    c

--- mainn.tf
+++ mainn.tf
@@ -2,13 +2,7 @@
 terraform {
   required_version = ">= 1.3"
   
-  backend "s3" {
-    bucket         = "omarmm32345"          # Your S3 bucket
-    key            = "eks/terraform.tfstate" # Path to the state file
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table" # Lock table
-    encrypt        = true
-  }
+  
 }
 
 # No resources defined
